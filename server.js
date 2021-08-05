d2697b385c4d063e2c4171a4cfe6aaa94a6c360c
Hannah Manfredi <hmanfredi@gmail.com>
2021-08-05 16:52:55 -0700

    Fix Proxy for both GET/POST reqs and its a hybrid forward/reverse proxy

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 const compression = require('compression');
 const express = require('express');
-const path = require('path');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const axios = require('axios');
-
+const parser = require('body-parser');
 const proxy = express();
 const port = 3000;
+const path = require('path');
+const cors = require('cors');
+const morgan = require('morgan');
+const createProxyMiddleware = require('http-proxy-middleware');
+const axios = require('axios');
+
+const service = "http://localhost:3004";
+//when deployed: service = ec2
 
 proxy.use(compression());
 proxy.use(express.static(path.join(__dirname, 'public')));
-proxy.use(bodyParser.json());
-proxy.use(bodyParser.urlencoded({extended: true}));
+proxy.use(morgan('dev'));
+proxy.use(parser.urlencoded({ extended: true}))
+proxy.use(express.json())
 proxy.use(cors());
 
 proxy.use((req, res, next) => {
@@ -20,23 +25,27 @@ proxy.use((req, res, next) => {
   next();
 });
 
-proxy.use(express.static(path.join(__dirname, "public")))
-
-// proxy.get('*/dp/:productId', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'public'));
-// });
-
-proxy.get(`http://localhost:3004/reviews/1`, (req, res) => {
-  res.sendFile(path.join(__dirname, 'public'))
+proxy.get('*/dp/:productId', (req, res) => {
+  res.sendFile(path.join(__dirname, '/./public/index.html'));
 });
 
-proxy.post('/newReview/:productid', (req, res) => {
+proxy.get(`/:productId`, (req, res) => {
+  res.sendFile(path.join(__dirname, `/./public/index.html`))
+});
+
+proxy.use('/reviews/:productid', createProxyMiddleware({
+  target: service,
+  changeOrigin: true
+  })
+);
+
+proxy.post('/newReview/:productid', (req, res) => { //'http://localhost:3000/'
   let review = req.body.body;
   let rndId = Math.floor(Math.random() * (1000000 - 900000) + 900000);
   axios.post(`http://localhost:3004/newReview/${rndId}`, JSON.stringify(review))
     .then((data) => {
-      console.log('PROXY DATA:', data);
-      res.sendFile(path.join(__dirname, 'public'));
+      console.log('PROXY Post DATA:', data);
+      res.status(201).send(JSON.stringify(data));
     })
     .catch((err) => {
       throw err;
